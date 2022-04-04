0add3b704fd2e1f03c6670dbbe3ee41eda6044b0
Nadja Reitzenstein <me@dequbed.space>
2022-04-04 20:08:56 +0200

    improve code for block1

--- 2: waking wakers & futuring futures/src/main.rs
+++ 2: waking wakers & futuring futures/src/main.rs
@@ -1,8 +1,12 @@
+// Our only dependencies are the Rust stdlib; you can compile and run this code just by calling
+// `$ rust block1.rs` and then run the resulting executable named `block1`
+
 use std::future::Future;
 use std::pin::Pin;
 use std::sync::{Arc, Weak, Mutex};
 use std::task::{Context, Waker, Poll};
 
+#[derive(Debug)]
 /// The shared contact point between tx and rx.
 /// 
 /// Both the TX and RX have a reference to an instance of this and "sending" a value is simply
@@ -10,7 +14,7 @@ use std::task::{Context, Waker, Poll};
 struct Shared<T> {
     /// Storage for a value that has be sent and not yet received
     pub slot: Option<T>,
-    /// The magical waker, I'll explain it just below ;)
+    /// The mystical Waker, the part making everything smartly async (◕▿◕✿)
     pub waker: Option<Waker>,
 }
 impl<T> Shared<T> {
@@ -20,23 +24,12 @@ impl<T> Shared<T> {
             waker: None,
         }
     }
-
-    /// Extract the internal value, if there is one.
-    ///
-    /// If there is no value currently, schedule the current task to be woken up when one is sent.
-    pub fn poll_get(&mut self, waker: &Waker) -> Option<T> {
-        if let Some(ref oldwaker) = self.waker {
-            if !waker.will_wake(oldwaker) {
-                self.waker.replace(waker.clone());
-            }
-        } else {
-            self.waker = Some(waker.clone());
-        }
-
-        self.slot.take()
-    }
 }
 
+#[derive(Debug)]
+/// Transmitting end
+///
+/// For simplicity this is a very simple and entirely syncronous implementation.
 struct TX<T> {
     shared: Weak<Mutex<Shared<T>>>,
 }
@@ -45,8 +38,8 @@ impl<T> TX<T> {
         Self { shared }
     }
 
-    /// Set the internal value, in essence "sending" it to the rx.
-    pub fn send(&self, value: T) -> Result<(), T> {
+    /// Filling the shared slot, in essence "sending" it to the rx.
+    pub fn try_send(&self, value: T) -> Result<(), T> {
         if let Some(shared) = self.shared.upgrade() {
             let mut guard = shared.lock().unwrap();
 
@@ -61,17 +54,54 @@ impl<T> TX<T> {
 
             Ok(())
         } else {
+            // This case is hit when the corresponding receiving end has been dropped. To not make
+            // things complicated we don't return a specific type for this kind of error.
             Err(value)
         }
     }
+
+    /// Returns true if the RX end has not yet been dropped.
+    pub fn is_rx_alive(&self) -> bool {
+        self.shared.strong_count() > 0
+    }
 }
 
+#[derive(Debug)]
 struct RX<T> {
     shared: Arc<Mutex<Shared<T>>>,
 }
 impl<T> RX<T> {
     pub fn new(shared: Arc<Mutex<Shared<T>>>) -> Self {
         Self { shared }
+    }
+
+    /// Extract the internal value, if there is one.
+    ///
+    /// If there is no value currently, **schedule the current task to be woken up** when one is sent.
+    pub fn poll_get(&self, waker: &Waker) -> Option<T> {
+        let mut guard = self.shared.lock().unwrap();
+
+        if let Some(ref oldwaker) = guard.waker {
+            if !waker.will_wake(oldwaker) {
+                guard.waker.replace(waker.clone());
+            }
+        } else {
+            guard.waker = Some(waker.clone());
+        }
+
+        guard.slot.take()
+    }
+}
+
+impl<T> Future for RX<T> {
+    type Output = T;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        if let Some(val) = self.poll_get(cx.waker()) {
+            return Poll::Ready(val);
+        } else {
+            return Poll::Pending;
+        }
     }
 }
 
@@ -82,20 +112,9 @@ fn channel<T>() -> (TX<T>, RX<T>) {
     (tx, rx)
 }
 
-impl<T> Future for RX<T> {
-    type Output = T;
-
-    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        let mut guard = self.shared.lock().unwrap();
-        if let Some(val) = guard.poll_get(cx.waker()) {
-            return Poll::Ready(val);
-        } else {
-            return Poll::Pending;
-        }
-    }
-}
-
 fn main() {
     let (tx, rx) = channel();
-    tx.send(42).unwrap();
+    tx.try_send(42).unwrap();
+    println!("I sent a value!");
+    println!("Now the endpoints look like this: {:?} and {:?}", tx, rx);
 }
